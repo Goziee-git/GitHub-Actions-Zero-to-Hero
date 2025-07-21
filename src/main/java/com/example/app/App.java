3c8e05d96c3bfb76085e700ac2ae923f05778040
Goziee-git <prospachigoz@gmail.com>
2025-07-21 11:16:07 +0100

    +src/ files for java and python

---
+++ src/main/java/com/example/app/App.java
@@ -0,0 +1,11 @@
+package com.example.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java GitHub Actions!");
+    }
+
+    public static String getGreeting() {
+        return "Hello from Java GitHub Actions!";
+    }
+}
