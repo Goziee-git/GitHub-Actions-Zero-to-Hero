b7e7a697bd5d6e28a0f8b25b2427a7c3cf025d86
Goziee-git <prospachigoz@gmail.com>
2025-07-22 19:52:19 +0100

    +mod gradle-ci.yml, app.java

--- java-with-gradle/src/main/java/com/example/app/Application.java
+++ java-with-gradle/src/main/java/com/example/app/Application.java
@@ -2,7 +2,7 @@ package com.example.app;
 
 public class Application {
     public String getGreeting() {
-        return "Hello, Prosper This is your JAVA Application!";
+        return "Hello, Prosper This is your Java Application for your github action test!";
     }
 
     public static void main(String[] args) {
